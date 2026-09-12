44e2f71f9984c2d4bfedb5b68070b667179baaba
Speedfire <vlad44812@gmail.com>
2026-09-13 00:16:19 +0200

    Adding printf implementation

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -2,6 +2,12 @@
 #include "stdio.h"
 
 void __attribute__((cdecl)) _cstart_(uint16_t bootDrive) {
-	puts("Hello underworld!");
-	for(;;){}
+
+	uint64_t a = 10;
+	uint64_t b = 10;
+	uint64_t c = a / b;
+
+	printf("Hello %s%s   %d", "wor", "ld!", c);
+
+	for (;;) {}
 }
--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -12,3 +12,96 @@ void puts(const char* str) {
 		str++;
 	}
 }
+
+int printf(const char* fmt, ...) {
+	int* argp = (int*)&fmt;
+	argp += sizeof(fmt) / sizeof(int);
+
+	int length = -1;
+	while (*fmt) {
+		if (*fmt == '%') {
+			fmt++;
+
+			if (*fmt >= '0' && *fmt <= '9') {
+				length = (*fmt) - '0';
+				fmt++;
+				while (*fmt >= '0' && *fmt <= '9') {
+					length *= 10;
+					length += (*fmt) - '0';
+					fmt++;
+				}
+			}
+
+			switch (*fmt) {
+			case 'd':
+			case 'i':;
+				int d = *argp;
+				argp++;
+
+				if (d < 0) {
+					putc('-');
+					d *= -1;
+				}
+				if (d == 0) {
+					putc('0');
+					break;
+				}
+
+				int tailingZeroes = 0;
+				while (d % 10 == 0) {
+					d /= 10;
+					tailingZeroes++;
+				}
+
+				int d2 = 0;
+				do {
+					d2 *= 10;
+					d2 += d % 10;
+					d /= 10;
+				} while (d > 0);
+
+				while (d2 > 0) {
+					putc(d2 % 10 + '0');
+					d2 /= 10;
+				}
+				for (;tailingZeroes > 0; tailingZeroes--) putc('0');
+
+				break;
+
+			case 'f':;
+				double f = *((double*)argp);
+				argp += sizeof(f) / sizeof(int);
+				break;
+
+			case 'c':;
+				char c = *((char*)argp);
+				argp += sizeof(c) / sizeof(int);
+
+				putc(c);
+
+				break;
+
+			case 's':;
+				const char* str = (const char*)*argp;
+				argp += sizeof(str) / sizeof(int);
+
+				while (*str && length != 0) {
+					putc(*str);
+					str++;
+					length--;
+				}
+
+				break;
+			}
+			fmt++;
+
+			length = -1;
+		}
+		else {
+			putc(*fmt);
+			fmt++;
+		}
+	}
+
+	return 0;
+}
