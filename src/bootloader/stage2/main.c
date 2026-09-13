cf43a3866304e340f98e6b3a70c4023bb738d0eb
Speedfire <vlad44812@gmail.com>
2026-09-13 02:36:16 +0200

    Adding CMake building and Visual studio support.

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -3,11 +3,7 @@
 
 void __attribute__((cdecl)) _cstart_(uint16_t bootDrive) {
 
-	uint64_t a = 10;
-	uint64_t b = 10;
-	uint64_t c = a / b;
-
-	printf("Hello %s%s   %d", "wor", "ld!", c);
+	printf("Hello %s%s   %d", "wor", "ld!", 55);
 
 	for (;;) {}
 }
